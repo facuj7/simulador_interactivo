552c39dbb1772835da563e2e21143351b0185da7
Facundo Juárez <facujuarez663@gmail.com>
2022-07-05 21:47:37 -0300

    primer proyecto Javascript

---
+++ index.js
@@ -0,0 +1,8 @@
+function descuento(num1, num2){
+    return num1 - num2;
+}
+
+let num1 = parseInt(prompt("ingrese el valor de su vivienda"));
+let num2 = parseInt(prompt("ingrese descuento pactado con vendedor"));
+let resultado = num1 - num2; 
+alert(resultado)
